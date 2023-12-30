aaf460802b6925a9cb5956b0df4a0c302223edc1
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2023-12-30 01:55:44 -0600

    Update KarpiWareV4 Loader.lua

--- Open Sourced/KarpiWareV4 Loader.lua
+++ Open Sourced/KarpiWareV4 Loader.lua
@@ -3,4 +3,4 @@
 _G.Theme = "Blue" -- Blue, Orange, Dark, Light, Purple, Red (You can also change theme within the command bar)
 _G.Prefix = ";" --  Key to open command bar
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/biggaboy212/KarpiWare/main/karpiwarev4src"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/biggaboy212/KarpiWare/main/karpiwarev4src.lua"))()
