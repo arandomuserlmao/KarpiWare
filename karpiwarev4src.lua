7c44b70a0a04ea533347e7e49d4360a87ee6e722
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2023-12-30 04:21:52 -0600

    Update karpiwarev4src.lua
    Added more TP locations

--- karpiwarev4src.lua
+++ karpiwarev4src.lua
@@ -40,6 +40,7 @@ local humanoid = character.Humanoid
 local target = nil
 local foundtarg = nil
 local mgogpos = nil
+local autofarm = false
 local Waypoints = {
 
 }
@@ -266,96 +267,73 @@ local Colours = {
     Away = ColorSequence.new(Color3.new(123, 123, 123), Color3.new(123, 123, 123))
 }
 
--- // Checks if the beam is hitting our character and can't go through walls
 local function IsBeamHit(Beam, MousePos)
-    -- // Get our character
     local Character = LocalPlayer.Character
     local Attachment = Beam.Attachment1
 
-    -- // Workout the direction
     local Origin = Beam.Attachment0.WorldPosition
     local Direction = MousePos - Origin
 
-    -- // Fire the ray, making sure it hits something (should unless aiming at sky?)
     local raycastParms = RaycastParams.new()
     raycastParms.FilterDescendantsInstances = {Character, Workspace.CurrentCamera}
-    local RaycastResult = Workspace:Raycast(Origin, Direction * 2, raycastParms) -- // Multiplied by 2 as ray might fall too short
+    local RaycastResult = Workspace:Raycast(Origin, Direction * 2, raycastParms)
     if (not RaycastResult) then
         Beam.Color = Colours.Away
         Attachment.WorldPosition = MousePos
         return
     end
 
-    -- // Set the colour based upon if aiming at us or not
     if (Character) then
         Beam.Color = RaycastResult.Instance:IsDescendantOf(Character) and Colours.At or Colours.Away
     end
 
-    -- // Set the position so the beam can't go through walls
     Attachment.WorldPosition = RaycastResult.Position
 end
 
--- // Creates a beam with the default properties
 local function CreateBeam(Character)
-    -- // Create beam
     local Beam = Instance.new("Beam", Character)
 
-    -- // Set important properties
     Beam.Attachment0 = Character:WaitForChild("Head"):WaitForChild("FaceCenterAttachment")
     Beam.Enabled = Character:FindFirstChild("GunScript", true) ~= nil
 
-    -- // Set visual properties
     Beam.Width0 = 0.1
     Beam.Width1 = 0.1
 
-    -- // Add to beam table so can modify all at once
     table.insert(Beams, Beam)
 
     -- // Return
     return Beam
 end
 
--- // Apply whenever we get a new character
 local function OnCharacter(Character)
-    -- // Make sure character exists
     if (not Character) then
         return
     end
 
-    -- // Wait for the MousePos
     local MousePos = Character:WaitForChild("BodyEffects"):WaitForChild("MousePos")
 
-    -- // Create the beam
     local Beam = CreateBeam(Character)
 
-    -- // Attachment for the mouse position
     local Attachment = Instance.new("Attachment", Terrain)
     Beam.Attachment1 = Attachment
-
-    -- // Constantly update when MousePos updates
     IsBeamHit(Beam, MousePos.Value)
     MousePos.Changed:Connect(function()
         IsBeamHit(Beam, MousePos.Value)
     end)
 
-    -- // See whenever they equip a gun
     Character.DescendantAdded:Connect(function(Descendant)
-        -- // Check if it is a gun, set enabled
         if (Descendant.Name == "GunScript") then
             Beam.Enabled = true
         end
     end)
 
-    -- // See whenever they unequip a gun
     Character.DescendantRemoving:Connect(function(Descendant)
-        -- // Check if it is a gun, set disabled
         if (Descendant.Name == "GunScript") then
             Beam.Enabled = false
         end
     end)
 end
 
--- // Apply on each player
 local function OnPlayer(Player)
     OnCharacter(Player.Character)
     Player.CharacterAdded:Connect(OnCharacter)
@@ -365,7 +343,6 @@ for _, v in ipairs(Players:GetPlayers()) do
     OnPlayer(v)
 end
 
--- // Redo for each new player
 Players.PlayerAdded:Connect(OnPlayer)
 end)
 
@@ -382,6 +359,43 @@ Window:AddCommand('AntiAutoKill', {}, 'Prevents people from autokilling you, Res
             part.Anchored = true
             part.Position = Vector3.new(-897.6600952148438, -650.0717163085938, -709.875732421875)
         character.HumanoidRootPart.CFrame = part.CFrame
+end)
+
+-- Cash ESP
+Window:AddCommand('Autofarm', {}, 'Cash autofarm, rejoin to stop', function(Arguments, Speaker)
+-- Revamped
+
+if autofarm == true then -- to stop from lagging
+else
+    autofarm = true
+    local humanoid = game.Players.LocalPlayer.Character.Humanoid
+    local tool = game.Players.LocalPlayer.Backpack.Combat
+    
+    local function Collect() 
+        task.wait(0.5)
+        for i, v in ipairs(game.Workspace.Ignored.Drop:GetChildren()) do
+            if v.Name == "MoneyDrop" and (v.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).magnitude <= 20 then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+                fireclickdetector(v.ClickDetector)
+                task.wait(0.5)
+            end  
+        end
+    end
+    
+    local function Start() 
+            humanoid:EquipTool(tool)
+            for i, v in ipairs(game.Workspace.Cashiers:GetChildren()) do
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Open.CFrame * CFrame.new(0, 0, 2)
+                for i = 0, 10 do
+                    task.wait(0.5)
+                    tool:Activate()
+                end
+            Collect()
+        task.wait(0.5)
+    end
+    end
+    Start()
+end
 end)
 
 -- Chams ESP
@@ -1287,7 +1301,7 @@ end)
 Window:AddCommand('MoneyGun', {}, 'Teleports to moneygun for SpawnCash Command', function(Arguments, Speaker)
 mgogpos = character.HumanoidRootPart.CFrame
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-885.24609375, 21.597984313964844, -129.31216430664062)
-wait(3)
+task.wait(3)
 character.HumanoidRootPart.CFrame = mgogpos
 end)
 
@@ -1298,10 +1312,10 @@ Window:AddCommand('CombatPhone', {}, 'Need Phone and Bat | Kill people with a ph
                    local PPname = "[Bat]"
 local PPlocation = game.Players.LocalPlayer.Character:WaitForChild(PPname)
 PPlocation.GripPos = Vector3.new(-100,-100,-100)
-wait(0.01)
+task.wait(0.01)
 plr1.Character["[Phone]"].Parent = plr1.Backpack
 plr1.Character["[Bat]"].Parent = plr1.Backpack
-wait(0.01)
+task.wait(0.01)
                   plr1.Backpack["[Phone]"].Parent = plr1.Character
                   plr1.Backpack["[Bat]"].Parent = plr1.Character
                    local PPname = "[Bat]"
@@ -1316,10 +1330,10 @@ Window:AddCommand('CombatFlowers', {}, 'Need Flowers and Bat | Kill people with 
      local PPname = "[Bat]"
 local PPlocation = game.Players.LocalPlayer.Character:WaitForChild(PPname)
 PPlocation.GripPos = Vector3.new(-100,-100,-100)
-wait(0.01)
+task.wait(0.01)
 plr1.Character["[FlowersTool]"].Parent = plr1.Backpack
 plr1.Character["[Bat]"].Parent = plr1.Backpack
-wait(0.01)
+task.wait(0.01)
     plr1.Backpack["[FlowersTool]"].Parent = plr1.Character
     plr1.Backpack["[Bat]"].Parent = plr1.Character
      local PPname = "[Bat]"
@@ -1334,10 +1348,10 @@ Window:AddCommand('CombatChicken', {}, 'Kill People with chicken', function(Argu
      local PPname = "[Bat]"
 local PPlocation = game.Players.LocalPlayer.Character:WaitForChild(PPname)
 PPlocation.GripPos = Vector3.new(-100,-100,-100)
-wait(0.01)
+task.wait(0.01)
 plr1.Character["[Chicken]"].Parent = plr1.Backpack
 plr1.Character["[Bat]"].Parent = plr1.Backpack
-wait(0.01)
+task.wait(0.01)
     plr1.Backpack["[Chicken]"].Parent = plr1.Character
     plr1.Backpack["[Bat]"].Parent = plr1.Character
      local PPname = "[Bat]"
@@ -1352,18 +1366,8 @@ Window:AddCommand('Bank', {}, 'Teleports to bank', function(Arguments, Speaker)
 end)
 
 
-Window:AddCommand('Revolver', {}, 'Teleports to revolver', function(Arguments, Speaker)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-643.6868286132812, 21.748022079467773, -121.32481384277344)
-end)
-
-
 Window:AddCommand('Police', {}, 'Teleports to Police station', function(Arguments, Speaker)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-265.4961853027344, 21.797977447509766, -116.37294006347656)
-end)
-
-
-Window:AddCommand('Drumgun', {}, 'Teleports to drumgun', function(Arguments, Speaker)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-81.06612396240234, 22.575956344604492, -86.40039825439453)
 end)
 
 
@@ -1382,11 +1386,6 @@ Window:AddCommand('Military', {}, 'Teleports to Military', function(Arguments, S
 end)
 
 
-Window:AddCommand('DBshotgun', {}, 'Teleports to DBshotgun', function(Arguments, Speaker)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(23.364553451538086, 25.628028869628906, -834.4078369140625)
-end)
-
-
 Window:AddCommand('PhoneStore', {}, 'Teleports to Phone Store', function(Arguments, Speaker)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-235.43539428710938, 22.065319061279297, -1121.0645751953125)
 end)
@@ -1402,18 +1401,8 @@ Window:AddCommand('Hospital', {}, 'Teleports to Hospital', function(Arguments, S
 end)
 
 
-Window:AddCommand('UphillGuns', {}, 'Teleports to UphillGuns', function(Arguments, Speaker)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(483.9078369140625, 48.06851577758789, -623.2725219726562)
-end)
-
-
 Window:AddCommand('Joses', {}, 'Teleports to Joses', function(Arguments, Speaker)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(583.9660034179688, 51.05943298339844, -479.64508056640625)
-end)
-
-
-Window:AddCommand('Bat', {}, 'Teleports to Bat', function(Arguments, Speaker)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-80.242919921875, 21.748022079467773, -293.10821533203125)
 end)
 
 
@@ -1432,13 +1421,128 @@ Window:AddCommand('Casino', {}, 'Teleports to Casino', function(Arguments, Speak
 end)
 
 
-Window:AddCommand('Sledgehammer', {}, 'Teleports to Sledgehammer', function(Arguments, Speaker)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-901.64404296875, 21.74802017211914, -296.5692443847656)
+Window:AddCommand('Club', {}, 'Teleports to Club', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-262.5336608886719, 22.628089904785156, -453.33203125)
+end)
+
+
+Window:AddCommand('School', {}, 'School', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-652.6968383789062, 21.982521057128906, 306.2254943847656)
+end)
+
+
+Window:AddCommand('UphillGuns', {}, 'Teleports to UphillGuns', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(483.9078369140625, 48.06851577758789, -623.2725219726562)
 end)
 
 
 Window:AddCommand('DownhillGuns', {}, 'Teleports to Downhill Guns', function(Arguments, Speaker)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-582.5549926757812, 8.312807083129883, -737.0886840820312)
+end)
+
+
+Window:AddCommand('RPG', {}, 'Teleports to RPG', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(114.18929290771484, -26.752010345458984, -276.26934814453125)
+end)
+
+
+Window:AddCommand('Revolver', {}, 'Teleports to Revolver', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-643.6868286132812, 21.748022079467773, -121.32481384277344)
+end)
+
+
+Window:AddCommand('DBshotgun', {}, 'Teleports to DBshotgun', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(23.364553451538086, 25.628028869628906, -834.4078369140625)
+end)
+
+
+Window:AddCommand('TacShotgun', {}, 'Teleports to Tactical Shotgun', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(23.364553451538086, 25.628028869628906, -834.4078369140625)
+end)
+
+
+Window:AddCommand('Rifle', {}, 'Teleports to Rifle', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-167.8747100830078, -18.040834426879883, -311.8492431640625)
+end)
+
+
+Window:AddCommand('AUG', {}, 'Teleports to AUG', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-273.3023376464844, 52.261661529541016, -216.994140625)
+end)
+
+
+Window:AddCommand('Knife', {}, 'Teleports to Knife', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-278.2847900390625, 21.74802017211914, -239.21456909179688)
+end)
+
+
+Window:AddCommand('Sledgehammer', {}, 'Teleports to Sledgehammer', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-901.64404296875, 21.74802017211914, -296.5692443847656)
+end)
+
+
+Window:AddCommand('Bat', {}, 'Teleports to Bat', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-80.242919921875, 21.748022079467773, -293.10821533203125)
+end)
+
+
+Window:AddCommand('Pitchfork', {}, 'Teleports to Pitchfork', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(250.8275604248047, 21.747995376586914, -28.456567764282227)
+end)
+
+
+Window:AddCommand('Shovel', {}, 'Teleports to Shovel', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(150.86940002441406, 21.747995376586914, 31.57048988342285)
+end)
+
+
+Window:AddCommand('StopSign', {}, 'Teleports to StopSign', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-225.90127563476562, 21.74802017211914, -81.81007385253906)
+end)
+
+
+Window:AddCommand('Tazer', {}, 'Teleports to Tazer', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-270.2383728027344, 21.7979793548584, -98.4942855834961)
+end)
+
+
+Window:AddCommand('TearGas', {}, 'Teleports to TearGas', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(98.40107727050781, 25.637149810791016, -891.5840454101562)
+end)
+
+
+Window:AddCommand('Lockpick', {}, 'Teleports to Lockpick', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-264.5516357421875, 21.748022079467773, -238.6057891845703)
+end)
+
+
+Window:AddCommand('Key', {}, 'Teleports to Key', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-271.192138671875, 21.748022079467773, -239.74179077148438)
+end)
+
+
+Window:AddCommand('Grenade', {}, 'Teleports to Grenade', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(108.70999145507812, -26.75200653076172, -273.833251953125)
+end)
+
+
+Window:AddCommand('Weights', {}, 'Teleports to Weights', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-55.78539276123047, 22.948291778564453, -654.3751220703125)
+end)
+
+
+Window:AddCommand('Lettuce', {}, 'Teleports to Lettuce', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-81.97262573242188, 22.698314666748047, -632.7714233398438)
+end)
+
+
+Window:AddCommand('Antibodies', {}, 'Teleports to Antibodies', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(109.00323486328125, 22.798017501831055, -471.5952453613281)
+end)
+
+
+Window:AddCommand('Mask', {}, 'Teleports to Mask', function(Arguments, Speaker)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(107.610595703125, 22.798015594482422, -486.6477966308594)
 end)
 
 
