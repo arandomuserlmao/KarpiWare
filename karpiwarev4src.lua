67c85e46d4293eeed49d1a665b5c542f8d70a0de
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-01-01 04:13:32 -0600

    Update karpiwarev4src.lua

--- karpiwarev4src.lua
+++ karpiwarev4src.lua
@@ -1,46 +1,5 @@
 _G.Prefix = ";" --  Key to open command bar
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local tablefind = table.find
-local MainEvent = ReplicatedStorage.MainEvent
-
-local Flags = {
-    "CHECKER_1",
-    "TeleportDetect",
-    "OneMoreTime"
-}
-
-local __namecall
-__namecall = hookmetamethod(game, "__namecall", function(...)
-
-    local args = {...}
-    local self = args[1]
-    local method = getnamecallmethod()
-
-    if (method == "FireServer" and self == MainEvent and tablefind(Flags, args[2])) then
-        return
-    end
-
-    if (not checkcaller() and getfenv(2).crash) then
-        local fenv = getfenv(2)
-        fenv.crash = function() end
-        setfenv(2, fenv)
-    end
-
-    -- //
-    return __namecall(...)
-end)
-
-local __newindex
-__newindex = hookmetamethod(game, "__newindex", function(t, k, v)
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        return
-    end
-
-    return __newindex(t, k, v)
-end)
-
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/VisualRoblox/Roblox/main/UI-Libraries/Visual%20Command%20UI%20Library/Source.lua', true))()
 
 local savedtheme = nil
